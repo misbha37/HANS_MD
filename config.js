16c96041a7f0f03a7760f0a40b0deb4eb123f215
HaroldMth <167818233+HaroldMth@users.noreply.github.com>
2025-02-01 14:56:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://ibb.co/zHFLSmdm",
+ALIVE_MSG: procass.enc.ALIVE_MSG || "Hans Byte is alive..."
 };
