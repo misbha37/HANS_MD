66e90f874c42a29ffbc8751cfa9b3ea115fb54cf
HaroldMth <167818233+HaroldMth@users.noreply.github.com>
2025-02-02 07:05:13 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,11 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://ibb.co/zHFLSmdm",
-ALIVE_MSG: procass.enc.ALIVE_MSG || "Hans Byte is alive..."
+SESSION_ID: process.env.SESSION_ID || "",  // Put your session id here <3
+ALIVE_IMG: process.env.ALIVE_IMG || "https://ibb.co/GvjHR1pp",
+ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *HansByte Md is AlIVE NOW....! 🖐🏻*",
+SUDO_NB: process.env.SUDO_NB || "237696900612",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+MODE: process.env.MODE || "public",
+AUTO_VOICE:"true"
 };
