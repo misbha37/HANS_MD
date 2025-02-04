408c37bb27d9b40cef277a6cadafed8aa7f003ad
HaroldMth <167818233+HaroldMth@users.noreply.github.com>
2025-02-04 02:56:57 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,4 +1,56 @@
 const {cmd , commands} = require('../command')
+const config = require('../config');
+
+// Fonction pour formater l'uptime en HH:MM:SS
+function getUptime() {
+    let totalSeconds = process.uptime(); // Récupère le temps en secondes depuis le démarrage
+    let hours = Math.floor(totalSeconds / 3600);
+    let minutes = Math.floor((totalSeconds % 3600) / 60);
+    let seconds = Math.floor(totalSeconds % 60);
+
+    return `${hours}h ${minutes}m ${seconds}s`;
+}
+
+// Stocke l'uptime dans une variable
+const uptime = getUptime();
+
+module.exports = { getUptime, uptime };
+
+// Fonction pour récupérer la date actuelle formatée en JJ/MM/AAAA
+function getCurrentDate() {
+    let date = new Date();
+    let day = String(date.getDate()).padStart(2, '0'); // Ajoute un zéro si < 10
+    let month = String(date.getMonth() + 1).padStart(2, '0'); // Mois commence à 0
+    let year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+}
+
+// Stocke la date actuelle dans une variable
+const currentDate = getCurrentDate();
+
+module.exports = { getCurrentDate, currentDate };
+
+
+// Fonction pour récupérer l'heure actuelle formatée en HH:MM:SS
+function getCurrentTime() {
+    let date = new Date();
+    let hours = String(date.getHours()).padStart(2, '0'); // Ajoute un zéro si < 10
+    let minutes = String(date.getMinutes()).padStart(2, '0');
+    let seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds}`;
+}
+
+// Stocke l'heure actuelle dans une variable
+const currentTime = getCurrentTime();
+
+module.exports = { getCurrentTime, currentTime };
+
+
+
+
+
 
 cmd({
     pattern: "menu",
@@ -14,6 +66,21 @@ let dec = `👋 HEY ${pushname} ,
 This is my awesome menu 🤫
 Consider joining my channel 
 > https://whatsapp.com/channel/0029VaZDIdxDTkKB4JSWUk1O
+
+
+╭━━〔 HANS BYTE X 〕━••═━━━❁ 
+│╭━━══─══━━⊛
+│║◆┊ *USERNAME* ➻ ${pushname}
+│║◆┊ *OWNER* ➻ ${config.OWNER_NAME}
+│║◆┊ *UPTIME* ➻ ${uptime}
+│║◆┊ *DEVS* ➻ *HANS TECH*
+│║◆┊ *DATE* ➻ ${currentDate}
+│║◆┊ *MODE* ➻ ${config.MODE}
+│║◆┊ *TIME* ➻ ${currentTime} GMT
+│║◆┊ *PLUGINS* ➻ 14
+┌┤
+┊╰─━━═••═━━••═━━••═━━⊛   
+╰⊷••HANS BYTE MD ✞••
 
 ╭════════════⊷❍
 │📌 LIST MENU
