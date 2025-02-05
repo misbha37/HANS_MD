476189bb26d53528e7bece725c91302e6ac468f6
HaroldMth <167818233+HaroldMth@users.noreply.github.com>
2025-02-05 03:26:47 +0100

    Hans Byte

--- config.js
+++ config.js
@@ -1,16 +1,15 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",  // DO NOT CHANGE THIS !!!
+SESSION_ID: process.env.SESSION_ID || "2VVSSToI#XbauT9Vb0dl9827ui3fFqUYELdMiLvVA0kdQza6b5yI",  // DO NOT CHANGE THIS !!!
 ALIVE_IMG: process.env.ALIVE_IMG || "https://ibb.co/GvjHR1pp",
 ALIVE_MSG: process.env.ALIVE_MSG || "HEY DEAR, I'm thrilled to announce that HansByte Md is ALIVE now—ready to dive into new adventures with you!",
 SUDO_NB: process.env.SUDO_NB || "237696900612",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "public",
 AUTO_VOICE:"true",
-OMDB_API_KEY: "5e339fb7"
+OMDB_API_KEY: "5e339fb7",
+OWNER_NAME: process.env.OWNER_NAME || "Harold Mth"
 };
